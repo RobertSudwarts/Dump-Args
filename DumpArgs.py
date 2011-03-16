f834289509fec1d6efba9598d2f290b3e1053ea7
Robert Sudwarts <robert.sudwarts@gmail.com>
2011-03-16 01:47:39 +0000

    first commit

---
+++ DumpArgs.py
@@ -0,0 +1,25 @@
+def dump_args(func):
+	'''this decorator dumps (to STDOUT) the arguments passed to a function before the decorated function is called itself
+	
+	.. note:: this function is safe for use with Sphinx documentation. ``echo_func.__doc__ = func.__doc__`` does the magic
+	'''
+	argnames = func.func_code.co_varnames[:func.func_code.co_argcount]
+	fname = func.func_name
+	
+	def echo_func(*args,**kw):
+		spcr = "*" * 100
+		print spcr
+		print ":: called by [function] : " + fname + " ::"
+		for arg, val in zip(argnames, args) + kw.items():
+			if arg=="self":
+				print arg, "=", type(val)
+			else:
+				print "%s=%r\t%s" % (arg, val, type(val))  # %r="raw"
+			
+		print spcr
+		return func(*args, **kw)
+	
+	# forces the calling function's docstring for use in sphinx documentation
+	echo_func.__doc__ = func.__doc__   
+	
+	return echo_func
